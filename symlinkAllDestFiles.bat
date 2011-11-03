fe0ddb4b51a356dc75f19744628634baeb3c60d7
Glen Robertson <phoglenix@gmail.com>
2011-11-03 20:59:16 +1300

    Updated to reduce chance of activating in system32

--- symlinkAllDestFiles.bat
+++ symlinkAllDestFiles.bat
@@ -1,12 +1,17 @@
 @echo off
 
+set steamDir=C:\Games\Steam\steamapps\
 set otherDir=I:\Games\Steam\steamapps\
 
+cd %steamDir%
 echo.
 echo The current directory is:
 cd
+echo      ^^^^ CHECK THIS ^^^^
+echo.
 echo This script will create symlinks in the current directory
 echo  pointing to each file/directory in %otherDir%
+echo                                      ^^^^ CHECK THIS ^^^^
 echo.
 echo It should not overwrite any existing files/links.
 echo It must run this as administrator to work.
